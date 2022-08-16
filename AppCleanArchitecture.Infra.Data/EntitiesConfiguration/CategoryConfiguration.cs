7dab10bc08ffebf290c97bc7f3cf1cc20a8434d8
Matheus Muniz <matheussk2015@gmail.com>
2022-08-15 22:10:44 -0300

    Implementando repositorios na camada de Infraestrutura.Data

---
+++ AppCleanArchitecture.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using AppCleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCleanArchitecture.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                     new Category(1, "Material"),
+                     new Category(2, "Eletronicos"),
+                     new Category(3, "Acessorios")
+                     );
+        }
+    }
+}
---
+++ AppCleanArchitecture.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using AppCleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCleanArchitecture.Infra.Data.EntitiesConfiguration
+{
+    class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(i => i.Id);
+
+            builder.Property(n => n.Name).HasMaxLength(100).IsRequired();
+            builder.Property(n => n.Description).HasMaxLength(200).IsRequired();
+
+            builder.Property(n => n.Price).HasPrecision(10,2);
+
+            builder.HasOne(n => n.Category).WithMany(e => e.Products).HasForeignKey(e => e.CategoryId);
+        }
+    }
+}
