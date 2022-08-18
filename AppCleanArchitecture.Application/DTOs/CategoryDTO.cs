4a012f3ad1700fa287f0249fd21865a894cab692
Matheus Muniz <matheussk2015@gmail.com>
2022-08-18 20:45:30 -0300

    Criação do Mapping com AutoMapper e DTO dos modelos com DataAnnotations

---
+++ AppCleanArchitecture.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppCleanArchitecture.Application.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Nome é obrigatorio")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
---
+++ AppCleanArchitecture.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,45 @@
+﻿using AppCleanArchitecture.Domain.Entities;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppCleanArchitecture.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O {0} é obrigatorio")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Nome")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "A {0} é obrigatorio")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Descrição")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "O {0} é obrigatorio")]
+        [Column(TypeName= "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Preço")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "O {0} é obrigatorio")]
+        [Range(1,9999)]
+        [DisplayName("Stock")]
+        public int Stock { get; set; }
+
+        [MaxLength(250)]
+        [DisplayName("Imagem do Produto")]
+        public string Image { get; set; }
+
+        [DisplayName("Categorias")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
