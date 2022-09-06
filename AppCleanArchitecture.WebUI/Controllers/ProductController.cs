45cf5a009ff5e6d5eab462d06150d7bd7c1fd06e
Matheus Muniz <matheussk2015@gmail.com>
2022-09-05 22:13:28 -0300

    Criação do CRUD na aplicação WEBUI

--- AppCleanArchitecture.WebUI/Controllers/ProductController.cs
+++ AppCleanArchitecture.WebUI/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
-﻿using AppCleanArchitecture.Application.Interfaces;
+﻿using AppCleanArchitecture.Application.DTOs;
+using AppCleanArchitecture.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,12 @@ namespace AppCleanArchitecture.WebUI.Controllers
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
-        public ProductController(IProductService productService)
+        private readonly ICategoryService _catergoryService;
+        public ProductController(IProductService productService,
+                                ICategoryService categoryService)
         {
             _productService = productService;
+            _catergoryService = categoryService;
         }
 
         [Route("Produtos-Disponiveis")]
@@ -21,6 +26,54 @@ namespace AppCleanArchitecture.WebUI.Controllers
         {
             var products = await _productService.GetProducts();
             return View(products);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = 
+                new SelectList(await _catergoryService.GetCategories(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDTO)
+        {
+            if(ModelState.IsValid)
+            {
+                await _productService.Add(productDTO);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDTO);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var productDTO = await _productService.GetById(id);
+
+            if (productDTO == null) return NotFound();
+
+            var categories = await _catergoryService.GetCategories();
+
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDTO.CategoryId);
+
+            return View(productDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                    await _productService.Update(productDTO);
+                    return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDTO);
         }
     }
 }
